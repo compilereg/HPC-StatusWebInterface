b66eb0405ca32f23a92e884634be69eb04be1c4d
Eng. Ahmed M. ElSayed <compiler@aast.edu>
2025-09-14 21:22:32 +0000

    Modify the list cluster paritions end point url

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.add_url_rule('/api/get/hoststate/<hostname>', view_func=ac.APIController.get
 app.add_url_rule('/api/get/hostmem/<hostname>', view_func=ac.APIController.getHostMemory)
 app.add_url_rule('/api/get/hostavgload/<hostname>', view_func=ac.APIController.getHostLoadAverage)
 app.add_url_rule('/api/get/hostprocesscount/<hostname>', view_func=ac.APIController.getHostProcesCount)
-app.add_url_rule('/api/get/clusterpartition/<hostname>', view_func=ac.APIController.getClusterParition)
+app.add_url_rule('/api/get/clusterpartitions/<hostname>', view_func=ac.APIController.getClusterParitions)
 
 
 if __name__ == '__main__':
